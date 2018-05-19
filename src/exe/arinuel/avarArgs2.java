5a1e69d7753b466e7a0eca2d9fbae3b8de37e129
Bartek Kowalski <bkowalski1975@gmail.com>
2018-05-19 19:06:39 +0200

    przeciążanie metod ze zmienną liczbą argumentów, klasa "avarArgs2"

---
+++ src/exe/arinuel/avarArgs2.java
@@ -0,0 +1,42 @@
+package exe.arinuel;
+
+
+public class avarArgs2 {
+
+    static void vaTest(int ... v) {
+        System.out.println("vaTest(int ...):" + "Liczba argumentów: " + v.length);
+        System.out.println("Contents: ");
+
+        for(int i = 0; i < v.length; i++)
+            System.out.println(" argument " + i + ": " + v[i]);
+
+        System.out.println();
+    }
+
+    static void vaTest(boolean ... v) {
+        System.out.println("vaTest(boolean ...):" + "Liczba argumentów: " + v.length);
+        System.out.println("Zawartość: ");
+
+        for(int i=0; i < v.length; i ++)
+            System.out.println(" argumetn " + i + ": " +v[i]);
+        System.out.println();
+    }
+
+    static void vaTest(String msg, int ... v) {
+        System.out.println("vaTest(String, int ...): " + msg + v.length);
+        System.out.println("Zawartość: ");
+
+        for(int i = 0; i < v.length; i++)
+            System.out.println(" argument " + i + ": " + v[i]);
+
+        System.out.println();
+    }
+
+
+    public static void main(String arg[]) {
+        vaTest(1, 2, 3);
+        vaTest("Testuję: ", 10, 20);
+        vaTest(true, false, false);
+
+    }
+}
